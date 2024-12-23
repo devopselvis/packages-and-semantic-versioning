396ccbac2ed991631c96ce8c2de7b22ae661459e
Mickey Gousset <mickeygousset@github.com>
2024-12-22 21:11:40 -0600

    feat: Add a "divide" function to the package Fixes #3

--- mickeys-awesome-package/src/MickeysAwesomePackage/Calculator.cs
+++ mickeys-awesome-package/src/MickeysAwesomePackage/Calculator.cs
@@ -6,5 +6,11 @@ namespace MickeysAwesomePackage
     {
       return a + b;
     }
+
+    //Create a new method called Divide that will divide two numbers
+    public static int Divide(int a, int b)
+    {
+      return a / b;
+    }
   }
 }
